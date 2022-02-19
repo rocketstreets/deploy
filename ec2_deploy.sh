bfd44ef242e7a224f08e5fd9db1d5a847267b460
dwipam <dwipam.katariya@gmail.com>
2022-02-19 17:53:14 -0500

    Add docker usr pwd

--- ec2_deploy.sh
+++ ec2_deploy.sh
@@ -4,4 +4,7 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 git clone https://github.com/rocketstreets/deploy.git
 cd deploy
+export DOCKER_USER=$DOCKER_USER
+export DOCKER_PWD=$DOCKER_PWD
 sh docker_deploy.sh
+
