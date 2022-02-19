954cf59f5cd0f49faadc64d1e6488c3352e129d7
dwipam <dwipam.katariya@gmail.com>
2022-02-19 16:34:52 -0500

    Add push

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+docker tag webapp-v0.0.0 dwipam/rocketstreets:webapp-v0.0.0
 docker push dwipam/rocketstreets:webapp-v0.0.0
+
+docker tag engine-v0.0.0 dwipam/rocketstreets:engine-v0.0.0
 docker push dwipam/rocketstreets:engine-v0.0.0
+
+docker tag robin_api-v0.0.0 dwipam/rocketstreets:robin_api-v0.0.0
 docker push dwipam/rocketstreets:robin_api-v0.0.0
