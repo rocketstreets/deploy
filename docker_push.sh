fc088dce9b5eb142c3f40a7ba6f86e560b6fd822
dwipam <dwipam.katariya@gmail.com>
2022-03-17 23:55:46 -0400

    Adding login to docker_push

--- docker_push.sh
+++ docker_push.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-docker tag webapp-v0.0.1 dwipam/rocketstreets:webapp-v0.0.1
-docker push dwipam/rocketstreets:webapp-v0.0.1
+echo "Pushing to docker hub version: "$1
+docker login -u $DOCKER_USER --password $DOCKER_PWD
+docker tag webapp-$1 dwipam/rocketstreets:webapp-$1
+docker push dwipam/rocketstreets:webapp-$1
 
-docker tag engine-v0.0.1 dwipam/rocketstreets:engine-v0.0.1
-docker push dwipam/rocketstreets:engine-v0.0.1
+docker tag engine-$1 dwipam/rocketstreets:engine-$1
+docker push dwipam/rocketstreets:engine-$1
 
-docker tag robin_api-v0.0.1 dwipam/rocketstreets:robin_api-v0.0.1
-docker push dwipam/rocketstreets:robin_api-v0.0.1
+docker tag robin_api-$1 dwipam/rocketstreets:robin_api-$1
+docker push dwipam/rocketstreets:robin_api-$1
+
+echo "##############################Done######################"
