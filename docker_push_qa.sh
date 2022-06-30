6a1d6a900ce8f9f98a65e304a5a8b16c5747220d
dwipam <dwipam.katariya@gmail.com>
2022-06-30 02:42:36 -0400

    Fix eligible to follow

--- docker_push_qa.sh
+++ docker_push_qa.sh
@@ -6,8 +6,8 @@ docker login -u $DOCKER_USER --password $DOCKER_PWD
 docker tag webapp-qa-$1 dwipam/rocketstreets:webapp-qa-$1
 docker push dwipam/rocketstreets:webapp-qa-$1
 
-docker tag webapp_v2-qa-$1 dwipam/rocketstreets:webapp_v2-qa-$1
-docker push dwipam/rocketstreets:webapp_v2-qa-$1
+#docker tag webapp_v2-qa-$1 dwipam/rocketstreets:webapp_v2-qa-$1
+#docker push dwipam/rocketstreets:webapp_v2-qa-$1
 
 docker tag engine-qa-$1 dwipam/rocketstreets:engine-qa-$1
 docker push dwipam/rocketstreets:engine-qa-$1
